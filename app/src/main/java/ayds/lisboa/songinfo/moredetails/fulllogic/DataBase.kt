f2c091ac23e87648f7314a22bd0e2ca73d01ffbd
FSassola <71612895+FSassola@users.noreply.github.com>
2023-04-25 22:39:30 -0300

    Migración a kotlin

---
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -0,0 +1,99 @@
+package ayds.lisboa.songinfo.moredetails.fulllogic
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+private const val DATABASE_NAME = "dictionary.db"
+private const val DATABASE_VERSION = 1
+
+private const val ARTIST_TABLE = "artists"
+private const val ID_COLUMN = "id"
+private const val ARTIST_COLUMN = "artist"
+private const val INFO_COLUMN = "info"
+private const val SOURCE_COLUMN = "source"
+private const val SORT_ORDER = "artist DESC"
+
+private const val createArtistTableQuery: String =
+  "create table $ARTIST_TABLE " +
+        "($ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        "$ARTIST_COLUMN string, " +
+        "$INFO_COLUMN string, " +
+        "$SOURCE_COLUMN integer)"
+
+internal class DataBase(context: Context ) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+  private val projection = arrayOf(
+    ID_COLUMN,
+    ARTIST_COLUMN,
+    INFO_COLUMN
+  )
+
+  @Override
+  override fun onCreate(db: SQLiteDatabase) {
+    db.execSQL(createArtistTableQuery)
+  }
+
+  @Override
+  override fun onUpgrade(db: SQLiteDatabase ,oldVersion: Int, newVersion: Int) {}
+
+  fun saveArtist(artist: String, info: String) {
+    val db: SQLiteDatabase = writableDatabase
+    val artistMap = getArtistInfoMap(artist, info)
+
+    db.insert(ARTIST_TABLE, null, artistMap)
+  }
+
+  private fun getArtistInfoMap(artist: String, info: String): ContentValues {
+    val values = ContentValues()
+
+    values.put(ARTIST_COLUMN, artist)
+    values.put(INFO_COLUMN, info)
+    values.put(SOURCE_COLUMN, 1)
+
+    return values
+  }
+
+  fun getInfo(artist: String): String {
+    val artistInfo: String
+
+    val artistsCursor = getArtistCursor(artist)
+    val artistsInfoList = getInfoFromCursor(artistsCursor)
+
+    artistInfo = artistsInfoList[0]
+    return artistInfo
+  }
+
+  private fun getArtistCursor(artist: String): Cursor {
+    return readableDatabase.query(
+      ARTIST_TABLE,
+      projection,
+      "$ARTIST_COLUMN = ?",
+      arrayOf(artist),
+      null,
+      null,
+      SORT_ORDER
+    )
+  }
+
+  private fun getInfoFromCursor(query: Cursor): List<String> {
+    val artistsInfo = ArrayList<String>()
+
+    try {
+      with(query) {
+        while(moveToNext()) {
+          val info = getString(getColumnIndexOrThrow(INFO_COLUMN))
+          artistsInfo.add(info)
+        }
+      }
+    }
+    catch (e: SQLException){
+      e.printStackTrace()
+    }
+
+    return artistsInfo
+  }
+}
