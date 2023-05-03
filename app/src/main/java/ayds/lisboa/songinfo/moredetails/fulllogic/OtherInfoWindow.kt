395789dfa7e6a539c04ecf10094483363f814092
diegojuarez965 <diegohernanjuarez965@gmail.com>
2023-05-03 09:21:40 -0300

    Corrección otro nivel de abstracción al acceder al intent en el onCreate

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -48,14 +48,13 @@ class OtherInfoWindow : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val artisName = intent.getStringExtra(ARTIST_NAME_EXTRA)
         setContentView(R.layout.activity_other_info)
 
         initProperties()
         initImageLoader()
         initDataBase()
         initLastFMAPI()
-        open(artisName)
+        getMoreDetailsOfAnArtistAsync()
     }
 
     private fun initProperties() {
@@ -76,17 +75,14 @@ class OtherInfoWindow : AppCompatActivity() {
         lastFMAPI = createLastFMAPI()
     }
 
-    private fun open(artist: String?) {
-        getMoreDetailsOfAnArtistAsync(artist)
-    }
-
-    private fun getMoreDetailsOfAnArtistAsync(artistName: String?) {
+    private fun getMoreDetailsOfAnArtistAsync() {
         Thread {
-            getMoreDetailsOfAnArtist(artistName)
+            getMoreDetailsOfAnArtist()
         }.start()
     }
 
-    private fun getMoreDetailsOfAnArtist(artistName: String?) {
+    private fun getMoreDetailsOfAnArtist() {
+        val artistName = intent.getStringExtra(ARTIST_NAME_EXTRA)
         val artistData = getArtistData(artistName)
         checkToInitializeTheButton(artistData)
         showArtistInfo(artistData.infoArtist)
