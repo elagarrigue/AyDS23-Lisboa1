55f7abe89c00d4d501738990ab4fcb5bfa1d5804
Federico Arias <ariasfede596@gmail.com>
2023-05-17 19:38:49 -0300

    Unificamos los test de ArtistInfoHelperImplTest

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistInfoHelperTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistInfoHelperTest.kt
@@ -8,20 +8,7 @@ class ArtistInfoHelperImplTest {
     private val artistInfoHelper: ArtistInfoHelper = ArtistInfoHelperImpl()
 
     @Test
-    fun `given text with special characters and term, it should return formatted HTML`() {
-        val text = "La canción High Hopes fue escrita por el grupo de rock británico Pink Floyd. Fue lanzada en 1994 como parte de su álbum The Division Bell."
-        val term = "Pink Floyd"
-
-        val result = artistInfoHelper.textToHtml(text, term)
-
-        val expected =
-            "<html><div width=400><font face=\"arial\">La canción High Hopes fue escrita por el grupo de rock británico <b>PINK FLOYD</b>. Fue lanzada en 1994 como parte de su álbum \"The Division Bell\".</font></div></html>"
-
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun `textToHtml should handle special characters and line breaks correctly`() {
+    fun `given text with the term, it should should handle special characters and line breaks correctly to return formatted HTML`() {
         val text = "Este es un ejemplo que contiene\nsaltos de líneas que deben\nrespetarse en el formato final\n devuelto"
         val term = "líneas"
 
@@ -33,4 +20,3 @@ class ArtistInfoHelperImplTest {
         assertEquals(expectedResult, result)
     }
 }
-
