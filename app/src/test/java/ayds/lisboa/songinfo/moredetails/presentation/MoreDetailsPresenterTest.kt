20b3703e6cf71eb9d38158b0d8643bb2b8f1f705
Federico Arias <ariasfede596@gmail.com>
2023-05-17 01:31:07 -0300

    Tester método principal del presenter
    Cuando nos piden encontrar la información del artista debemos corroborar que una vez encontrado el Artist, independientemente si es EmptyArtist o ArtistData, debemos notificar a quien se haya suscripto a nuestro observable con el UIState actualizado

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenterTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenterTest.kt
@@ -1,4 +1,37 @@
 package moredetails.presentation
 
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
+import ayds.lisboa.songinfo.moredetails.domain.repository.ArtistRepository
+import ayds.lisboa.songinfo.moredetails.presentation.ArtistInfoHelper
+import ayds.lisboa.songinfo.moredetails.presentation.MoreDetailsPresenter
+import ayds.lisboa.songinfo.moredetails.presentation.MoreDetailsPresenterImpl
+import ayds.lisboa.songinfo.moredetails.presentation.MoreDetailsUiState
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class MoreDetailsPresenterTest {
+    private val repository : ArtistRepository = mockk()
+    private val helper: ArtistInfoHelper = mockk(relaxUnitFun = true)
+
+    private val moreDetailsPresenter: MoreDetailsPresenter by lazy {
+        MoreDetailsPresenterImpl(repository, helper)
+    }
+
+    @Test
+    fun `on get artist information, it should notify the result`() {
+        val artistName = "artist"
+        val artistData: Artist = mockk()
+        every { repository.getArtistData(artistName) } returns artistData
+
+        val artistTester: (MoreDetailsUiState) -> Unit = mockk(relaxed = true)
+        moreDetailsPresenter.artistObservable.subscribe() {
+            artistTester(it)
+        }
+
+        moreDetailsPresenter.getArtistMoreInformation(artistName)
+
+        verify { artistTester(any()) }
+    }
 }
