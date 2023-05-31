18817a2fa1dc2571a3e981b8297ece41a92514ef
Federico Arias <ariasfede596@gmail.com>
2023-05-31 13:11:24 -0300

    Corrección equivocación de merge

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/data/repository/local/CursorToArtistDataMapper.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/data/repository/local/CursorToArtistDataMapper.kt
@@ -1,6 +1,7 @@
 package ayds.lisboa.songinfo.moredetails.data.repository.local
 
 import android.database.Cursor
+import ayds.lisboa.songinfo.moredetails.domain.entities.Card
 import ayds.lisboa.songinfo.moredetails.domain.entities.Card.CardData
 import java.sql.SQLException
 
@@ -13,17 +14,14 @@ internal class CursorToArtistDataMapperImpl : CursorToArtistDataMapper {
     override fun map(cursor: Cursor): CardData? =
         try {
             with(cursor) {
-                if(moveToNext()) {
                     CardData(
                         artistName = getString(getColumnIndexOrThrow(ARTIST_COLUMN)),
                         description = getString(getColumnIndexOrThrow(DESCRIPTION_COLUMN)),
                         infoURL = getString(getColumnIndexOrThrow(INFO_URL_COLUMN)),
+                        source = Card.Source.values()[getColumnIndexOrThrow(SOURCE_COLUMN)],
+                        sourceLogoURL = getString(getColumnIndexOrThrow(SOURCE_LOGO_COLUMN)),
                     )
                 }
-                else {
-                    null
-                }
-            }
         } catch (e: SQLException) {
             e.printStackTrace()
             null
