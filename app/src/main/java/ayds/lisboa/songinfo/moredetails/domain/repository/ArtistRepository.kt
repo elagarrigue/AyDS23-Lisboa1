e8a84cdb87c40094c2864d994a029f672d2af55f
diegojuarez965 <diegohernanjuarez965@gmail.com>
2023-05-31 22:18:44 -0300

    Arreglo de import y condicion para marcar como local

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/repository/ArtistRepository.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/repository/ArtistRepository.kt
@@ -1,6 +1,6 @@
 package ayds.lisboa.songinfo.moredetails.domain.repository
 
-import ayds.lisboa.songinfo.moredetails.domain.entities.Card
+import ayds.lisboa.songinfo.moredetails.domain.entities.Card.CardData
 
 interface ArtistRepository {
     fun getArtistData(artistName: String) : List<CardData>
